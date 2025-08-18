e308ad8d7405476b5a61d140a9e5a69814a01ebd
Welder Victor <weldersalvadorvictor12@hotmail.com>
2025-08-17 23:04:11 -0300

    Descrpition of the problem

--- Differential_Equations/PositionProblem.py
+++ Differential_Equations/PositionProblem.py
@@ -1,5 +1,13 @@
 from sympy import symbols,diff,lambdify
 import sympy as sp
+
+"""Description: The function below returns a zero of a real function. The implementation has the physical
+motivation of a free fall with air resistance, resulting in a differential equation with the form of
+the variable "f"(Example 2 of the chapter one in the book "elementary differential equations and boundary
+value problems. William E.Boyce/Richard C.DiPrima"). We want to know when (for wich t = x) the position of
+the ball has value of 300, knowing that the inicial velocity is zero. It was used the Newthon Method
+for reaching the zero value of the function"""
+
 x = symbols('x')
 f = 49*x + 245*sp.exp(-x/5) - 545
 f_function = lambdify(x,f)
@@ -16,6 +24,6 @@ def zero(x0,max = 10):
         k += 1
     return solutions[k - 1]
 
-print("Zero da função: {}".format(zero(3)))
+print("Root: {}".format(zero(3)))
 
 
