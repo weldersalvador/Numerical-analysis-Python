b93e3164d3941a10d0cbbcba65a4eae2f1de963d
Welder Victor <weldersalvadorvictor12@hotmail.com>
2025-08-17 22:44:39 -0300

    Resolvendo problemas
    Foram resolvidos os problemas de lógica.

--- Differential_Equations/PositionProblem.py
+++ Differential_Equations/PositionProblem.py
@@ -1,24 +1,21 @@
 from sympy import symbols,diff,lambdify
-import numpy as np
-
+import sympy as sp
 x = symbols('x')
-f = x - x**(1/3) - 2
+f = 49*x + 245*sp.exp(-x/5) - 545
 f_function = lambdify(x,f)
 f_prime = diff(f,x)
 f_prime_function =  lambdify(x,f_prime)
 
 
-def zero(x0,x1,precision = 0.001,max = 100):
-    prime = f_prime_function
+def zero(x0,max = 10):
     solutions = []
     k = 1
-    solutions.append(f_function(x0))
-    solutions.append(f_function(x1))
-    while(f_function(solutions[k]) > precision or k == max):
-        solutions.append(solutions[k] - (f_function(solutions[k - 1])/prime(solutions[k-1])))
+    solutions.append(x0)
+    while(k < max):
+        solutions.append(solutions[k - 1] - (f_function(solutions[k - 1])/f_prime_function(solutions[k-1])))
         k += 1
-    return solutions[k]
+    return solutions[k - 1]
 
-print("Zero da função: {}".format(zero(3,5)))
+print("Zero da função: {}".format(zero(3)))
 
 
